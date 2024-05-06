3b1a4b53e4b2c065680ec730bc219456914f72af
Sofia <s.ahmed9833@gmail.com>
2024-05-06 13:06:06 +0100

    Added more test cases (currently failing)

--- FizzBuzzSOLID.Service.Test/FizzBuzzServiceTests.cs
+++ FizzBuzzSOLID.Service.Test/FizzBuzzServiceTests.cs
@@ -14,6 +14,13 @@ public class FizzBuzzServiceTests
 
     [TestCase(15, "Fizzbuzz")]
     [TestCase(3, "Fizz")]
+    [TestCase(2, "2")]
+    [TestCase(20, "Buzz")]
+    [TestCase(32, "32")]
+    [TestCase(60, "Fizzbuzz")]
+    [TestCase(-18, "Fizz")]
+    [TestCase(0, "0")]
+    [TestCase(333, "Fizz")]
     public void CalculateFizzBuzzResult(int number, string expectedResult)
     {
         var actualResult = _fizzBuzzService.CalculateFizzBuzzResult(number);
